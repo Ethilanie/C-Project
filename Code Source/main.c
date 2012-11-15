ba2b997f5c81e14e647277d68fb6c4c3026cc690
alexandreguichon <alexandreguichon@gmail.com>
2012-11-15 14:34:39 +0100

    m

--- Code Source/main.c
+++ Code Source/main.c
@@ -1,12 +1,1 @@
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-
-struct _CL{
-    int val;
-    struct _CL* suiv;
-};
-
-typedef struct _CL CL;
-
-typedef CL* liste;
+#include "fonctions.h"
