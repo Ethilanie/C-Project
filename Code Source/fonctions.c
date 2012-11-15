9de8a70645e99a679ee7deb4fcee46bad44e8b8b
alexandreguichon <alexandreguichon@gmail.com>
2012-11-15 16:11:15 +0100

    fct nbpiles.c

--- Code Source/fonctions.c
+++ Code Source/fonctions.c
@@ -8,30 +8,6 @@
 
 CL* recherche(int valeur, CL skipList){
 
-<<<<<<< HEAD
-int nbpiles(float prob){
-    int face = 0;
-    int res = -1;
-    float n;
-    while(face==0){
-        res++;
-        n = rand()%(100);
-        if(n>=prob*100){
-            face=1;
-        }
-    }
-    return res;
-}
-
-=======
-if(skipList==NULL) return NULL;
-
-
-
-
-
-
-}
 
 
 
