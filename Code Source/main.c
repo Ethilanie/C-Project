01d7a3cd58d0c1cafb247ab15e6e8552e426e51f
Aethia <elise.richard@live.fr>
2012-11-15 16:01:36 +0100

    options

--- Code Source/main.c
+++ Code Source/main.c
@@ -1,6 +1,30 @@
 #include "fonctions.h"
-
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char* argv[]){
 
+int opt, nbopt;
+int nbVal;
+float proba;
+
+nbopt=1;
+while ((opt = getopt(argc, argv, "tn:")) != -1) {
+	switch (opt) {
+		case 't':
+			//si t, option sans argument 
+			nbopt++;
+			//printf("lol");
+			break;
+			
+        case 'n': //nombre d'éléments a inserer
+            printf("%s", argv[nbopt+1]);
+			nbopt=nbopt+1;
+			
+            break;
+		
+
+	}
+}
 }
